2a2dbde00ed46d097a20cdadf7568710f377c427
prabh <prabhjeetsokhey@gmail.com>
2023-01-17 23:03:00 -0500

    added Command struct following design doc.

--- include/command.h
+++ include/command.h
@@ -2,9 +2,16 @@
 
 #ifndef OPEN_COMMAND_H
 #define OPEN_COMMAND_H
-
-struct Command {
-
+typedef struct Command {
+    char *line; // The command line for this command
+    char* command; // The command (e.g. ls, exit, cd, cat)
+    int argc; // The number of arguments passed to the command
+    char** argv; // The arguments passed to the command
+    int stdin_file; // The file to redirect stdin from, or NULL
+    int stdout_file; // The file to redirect stdout to, or NULL
+    int std_overwrite; // Append to or truncate the stdout file
+    int stderr_overwrite; // Append to or truncate the stderr file
+    int exit_code; // The status returned from the command
 };
 
 #endif //OPEN_COMMAND_H
