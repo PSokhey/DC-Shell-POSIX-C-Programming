ca7bf4cef0267c49b346b4bedc93aeaf25454ecf
prabh <prabhjeetsokhey@gmail.com>
2023-01-17 21:32:25 -0500

    entered states for the shell program.

--- include/state.h
+++ include/state.h
@@ -3,4 +3,17 @@
 #ifndef OPEN_STATE_H
 #define OPEN_STATE_H
 
+// States to be used for shell.
+enum state {
+    INIT_STATE, // Initial shell state.
+    READ_COMMANDS, // Reading command state.
+    SEPARATE_COMMANDS, // Tokenize teh comm.
+    PARSE_COMMANDS, // Parse the commands to shell for next action.
+    EXECUTE_COMMANDS, // Execute the command that was parsed.
+    RESET_STATE, // Clean memory and get ready for reading again.
+    ERROR, // Error exit gracefully.
+    EXIT, // Exit the program
+    DESTROY_STATE // Clean memory before closing program.
+};
+
 #endif //OPEN_STATE_H
--- src/main.c
+++ src/main.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LENGTH 1024
+
 
 int main(){
+
+    // for holding input from user.
+    char input[MAX_LENGTH];
+
+    // reading input from user.
+    printf(">>");
+    fgets(input, MAX_LENGTH, stdin);
+
+    printf("you entered %s",input);
 
 
     return 0;
