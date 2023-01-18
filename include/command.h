46e908d4011582d45d45876a94121f85d59ed470
prabh <prabhjeetsokhey@gmail.com>
2023-01-18 12:17:26 -0500

    overwrite in Command structs should be boolean to check if they could be overwritten or not.

--- include/command.h
+++ include/command.h
@@ -2,6 +2,10 @@
 
 #ifndef OPEN_COMMAND_H
 #define OPEN_COMMAND_H
+
+#include <stdbool.h>
+
+// Pointers might change to char*.
 struct Command {
     char *line; // The command line for this command
     char* command; // The command (e.g. ls, exit, cd, cat)
@@ -9,8 +13,8 @@ struct Command {
     char** argv; // The arguments passed to the command
     int stdin_file; // The file to redirect stdin from, or NULL
     int stdout_file; // The file to redirect stdout to, or NULL
-    int std_overwrite; // Append to or truncate the stdout file
-    int stderr_overwrite; // Append to or truncate the stderr file
+    bool std_overwrite; // Append to or truncate the stdout file
+    bool stderr_overwrite; // Append to or truncate the stderr file
     int exit_code; // The status returned from the command
 };
 
