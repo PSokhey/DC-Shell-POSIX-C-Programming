78f3d29eb4bc3efd5032b72f838d9a275f073d2c
prabh <prabhjeetsokhey@gmail.com>
2023-01-22 18:54:32 -0500

    change fields of command struct.

--- include/command.h
+++ include/command.h
@@ -11,10 +11,11 @@
 struct command {
     char *line; // The command line for this command
     char* command; // The command (e.g. ls, exit, cd, cat)
-    int argc; // The number of arguments passed to the command
+    size_t argc; // The number of arguments passed to the command
     char** argv; // The arguments passed to the command
-    int stdin_file; // The file to redirect stdin from, or NULL
-    int stdout_file; // The file to redirect stdout to, or NULL
+    char* stdin_file; // The file to redirect stdin from, or NULL
+    char* stdout_file; // The file to redirect stdout to, or NULL
+    char* stderror_file;
     bool std_overwrite; // Append to or truncate the stdout file
     bool stderr_overwrite; // Append to or truncate the stderr file
     int exit_code; // The status returned from the command
